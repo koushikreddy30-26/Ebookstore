6f180a8b49889feec5e61ffa0d54578e7edd8d35
koushikreddy30-26 <koushikreddybasireddy05@gmail.com>
2025-10-31 00:02:54 +0530

    Fix production build path for Render deployment

--- server/server.js
+++ server/server.js
@@ -46,8 +46,8 @@ app.use('/api/orders', orderRoutes);
 // Serve frontend for production
 if (process.env.NODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/client/build')));
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client/build/index.html')));
 } else {
   // Route for development
   app.get('/', (req, res) => res.send('API is running...'));
